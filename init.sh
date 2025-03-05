3963a0dd147b9b9cefc065711df238ca413f1baa
borisrosedev <boris.rose.dev@gmail.com>
2025-03-05 23:41:35 +0100

    feat/dev/v1

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+mkdir -p src/ts src/sass data assets docs
+touch README.md
+touch ./data/users.json
+npm install -D webpack webpack-cli webpack-dev-server
+npm install -D sass sass-loader css-loader style-loader ts-loader
+npm install -D prettier && touch .prettierrc.json
+npm install -D eslint @eslint/js typescript typescript-eslint
+npm install -D jest
+npm install -D babel-jest @babel/core @babel/preset-env
+npm install --save-dev @types/jest
+npm install --save-dev jest-environment-jsdom
